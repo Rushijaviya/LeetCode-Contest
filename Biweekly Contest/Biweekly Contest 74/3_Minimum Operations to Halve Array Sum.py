0dfe91b37ec2f09c50a70bfa57f4ced8d79dc08f
unknown <201951074@iiitvadodara.ac.in>
2022-03-28 22:44:23 +0530

    Minimum Operations to Halve Array Sum

---
+++ Biweekly Contest/Biweekly Contest 74/3_Minimum Operations to Halve Array Sum.py
@@ -0,0 +1,19 @@
+# 2208. Minimum Operations to Halve Array Sum
+# https://leetcode.com/problems/minimum-operations-to-halve-array-sum/
+
+# from heapq import heapify, heappop, heappush
+
+
+class Solution:
+    def halveArray(self, nums):
+        half=sum(nums)/2
+        count=0
+        heap=[-i for i in nums]
+        heapify(heap)
+        while half>0:
+            temp=heappop(heap)
+            temp/=2
+            half+=temp
+            heappush(heap,temp)
+            count+=1
+        return count
