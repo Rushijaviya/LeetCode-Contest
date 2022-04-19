70ad22c5f82f61fc938e8f075ee5d224c74fb299
unknown <201951074@iiitvadodara.ac.in>
2022-04-19 11:57:57 +0530

    Minimum Rounds to Complete All Tasks

--- Weekly Contest/Weekly Contest 289/2_Minimum Rounds to Complete All Tasks.py
+++ Weekly Contest/Weekly Contest 289/2_Minimum Rounds to Complete All Tasks.py
@@ -25,3 +25,8 @@ class Solution:
                 if rem==1:
                     ans+=(div-1+2)  
         return ans
+
+        '''
+        freq = Counter(tasks).values()
+        return -1 if 1 in freq else sum((a + 2) // 3 for a in freq)
+        '''
