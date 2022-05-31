2976ea5214299606c8f4a281183ff52939f0f713
unknown <201951074@iiitvadodara.ac.in>
2022-05-31 23:56:33 +0530

    Maximum Consecutive Floors Without Special Floors

---
+++ Weekly Contest/Weekly Contest 293/2_Maximum Consecutive Floors Without Special Floors.py
@@ -0,0 +1,6 @@
+# 2274. Maximum Consecutive Floors Without Special Floors
+# https://leetcode.com/problems/maximum-consecutive-floors-without-special-floors/
+
+class Solution:
+    def maxConsecutive(self, bottom, top, special):
+        pass
