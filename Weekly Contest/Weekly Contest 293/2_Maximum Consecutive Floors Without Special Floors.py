d8eaa724ab32c25364d37b1fec7e6f989f98526d
unknown <201951074@iiitvadodara.ac.in>
2022-06-01 23:56:38 +0530

    Maximum Consecutive Floors Without Special Floors

--- Weekly Contest/Weekly Contest 293/2_Maximum Consecutive Floors Without Special Floors.py
+++ Weekly Contest/Weekly Contest 293/2_Maximum Consecutive Floors Without Special Floors.py
@@ -3,4 +3,9 @@
 
 class Solution:
     def maxConsecutive(self, bottom, top, special):
-        pass
+        special.sort()
+        n=len(special)
+        ans=special[0]-bottom
+        for i in range(1,n):
+            ans=max(ans,special[i]-special[i-1]-1)
+        return max(ans,top-special[-1])
