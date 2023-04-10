dff738c99cb01c0660b11900cff834d459ff2434
Rushi Javiya <201951074@iiitvadodara.ac.in>
2023-04-11 00:26:10 +0530

    Minimum Operations to Make All Array Elements Equal

--- Weekly Contest/Weekly Contest 338/3_Minimum Operations to Make All Array Elements Equal.py
+++ Weekly Contest/Weekly Contest 338/3_Minimum Operations to Make All Array Elements Equal.py
@@ -1,4 +1,4 @@
-# 2602. Minimum Operations to Make All Array Elements Equal   
+# 2602. Minimum Operations to Make All Array Elements Equal
 # https://leetcode.com/problems/minimum-operations-to-make-all-array-elements-equal/
 
 from bisect import bisect_left
