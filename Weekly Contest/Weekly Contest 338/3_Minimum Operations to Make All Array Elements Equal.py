e4b8b5786dd799178b8e6593f9d096860a58fb2e
Rushi Javiya <201951074@iiitvadodara.ac.in>
2023-04-11 00:25:33 +0530

    Minimum Operations to Make All Array Elements Equal

---
+++ Weekly Contest/Weekly Contest 338/3_Minimum Operations to Make All Array Elements Equal.py
@@ -0,0 +1,26 @@
+# 2602. Minimum Operations to Make All Array Elements Equal   
+# https://leetcode.com/problems/minimum-operations-to-make-all-array-elements-equal/
+
+from bisect import bisect_left
+from itertools import accumulate
+
+class Solution:
+    def minOperations(self, nums, queries):
+        '''
+        # TLE
+        n=len(queries)
+        ans=[0]*n
+        for num in nums:
+            for i in range(n):
+                ans[i]+=abs(queries[i]-num)
+        return ans
+        '''
+
+        nums.sort()
+        pref=[0]+list(accumulate(nums))
+        ans=[]
+        for qur in queries:
+            idx=bisect_left(nums,qur)
+            temp=qur*(idx)-pref[idx]+(pref[-1]-pref[idx])-qur*(len(nums)-(idx))
+            ans.append(temp)
+        return ans
